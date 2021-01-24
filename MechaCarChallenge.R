300194916f10160fcaa1b1e9a3ff5a04e25232ae
mayajaral <35385165+mayajaral@users.noreply.github.com>
2021-01-24 20:40:44 +0530

    analysis complete, readme pending

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,5 +7,16 @@ summary(lm(mpg ~ vehicle_weight,data=MC_mpg))
 summary(lm(mpg ~ spoiler_angle,data=MC_mpg)) 
 summary(lm(mpg ~ ground_clearance,data=MC_mpg)) 
 summary(lm(mpg ~ AWD,data=MC_mpg))
-summary(lm(mpg ~ mpg,data=MC_mpg)) 
+
+sus_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- sus_coil %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) 
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
+
+t.test(sus_coil$PSI,mu=1500) #compare sample versus population means
+t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot1",select= PSI )) #compare sample versus population means
+t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot2",select= PSI )) #compare sample versus population means
+t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot3",select= PSI )) #compare sample versus population means
+
+
 
