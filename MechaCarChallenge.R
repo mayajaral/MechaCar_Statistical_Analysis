7cf517a2e9c0f39a6374b0785c890a3f55f7a8bf
mayajaral <35385165+mayajaral@users.noreply.github.com>
2021-02-10 22:36:17 +0530

    challenge corrected

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,11 +2,13 @@ library("dplyr")
 # Read in CSV
 MC_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 # Simple Linear Regression
-summary(lm(mpg ~ vehicle_length,data=MC_mpg)) 
-summary(lm(mpg ~ vehicle_weight,data=MC_mpg))
-summary(lm(mpg ~ spoiler_angle,data=MC_mpg)) 
-summary(lm(mpg ~ ground_clearance,data=MC_mpg)) 
-summary(lm(mpg ~ AWD,data=MC_mpg))
+#summary(lm(mpg ~ vehicle_length,data=MC_mpg)) 
+#summary(lm(mpg ~ vehicle_weight,data=MC_mpg))
+#summary(lm(mpg ~ spoiler_angle,data=MC_mpg)) 
+#summary(lm(mpg ~ ground_clearance,data=MC_mpg)) 
+#summary(lm(mpg ~ AWD,data=MC_mpg))
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MC_mpg)) 
 
 # Summary 
 sus_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
@@ -14,11 +16,11 @@ sus_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors =
 total_summary <- sus_coil %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) 
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep') 
 
-## T-test
-t.test(sus_coil$PSI,mu=1500) #compare sample versus population means
-t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot1",select= PSI )) #compare sample versus population means
-t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot2",select= PSI )) #compare sample versus population means
-t.test(sus_coil$PSI,mu=1500, subset(sus_coil, Manufacturing_Lot=="Lot3",select= PSI )) #compare sample versus population means
 
+
+## Correct Syntax
+t.test(subset(sus_coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(sus_coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(sus_coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
 
 
